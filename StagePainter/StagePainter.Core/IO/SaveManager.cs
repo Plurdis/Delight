bf933b36910b34e7d5cdaa7b76aaaf9cf6f2a352
Plurdis <uutak2000@naver.com>
2018-06-14 00:18:19 +0900

    SaveData Func Additional Implement

--- StagePainter/StagePainter.Core/IO/SaveManager.cs
+++ StagePainter/StagePainter.Core/IO/SaveManager.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using StagePainter.Core.Exceptions;
+using StagePainter.Core.Extension;
+
 namespace StagePainter.Core.IO
 {
     /// <summary>
@@ -16,12 +19,47 @@ namespace StagePainter.Core.IO
         /// </summary>
         /// <param name="data">Data that save to file that have <see cref="SerializableAttribute"/> Attribute</param>
         /// <param name="fileLocation">Input where you save file.</param>
+        /// <exception cref="AttributeNotFoundException"/>
+        /// <exception cref="SaveFailException"/>
         /// <returns></returns>
-        public static bool SaveFile<T>(this object data, string fileLocation) where T : class
+        public static bool SaveData<T>(this object data, string fileLocation) where T : class
         {
-            System.IO.File.WriteAllBytes(fileLocation, null);
+            var attr = data.GetAttribute<StorageMethodAttribute>();
+
+            if (attr == null)
+                throw new AttributeNotFoundException();
+
+            string formattedText = FormatText(attr.StorageMethodType);
+
+            try
+            {
+                System.IO.File.WriteAllBytes(fileLocation, attr.EncodingType.GetBytes(formattedText));
+            }
+            catch (Exception ex)
+            {
+                throw new SaveFailException("Can't save file. See InnerException for details.", ex);
+            }
 
             return true;
+        }
+
+        public static string FormatText(StorageMethodTypes type)
+        {
+            switch (type)
+            {
+                case StorageMethodTypes.XML:
+                    break;
+                case StorageMethodTypes.Json:
+                    break;
+                case StorageMethodTypes.Serialize:
+                    break;
+                case StorageMethodTypes.Custom:
+                    break;
+                default:
+                    break;
+            }
+
+            return string.Empty;
         }
     }
 }
