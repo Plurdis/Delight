f3cc9295775f9e382ab08452efebb67efd26f1cc
Plurdis <uutak2000@naver.com>
2018-06-14 00:14:05 +0900

    Add Exceptions (AttributeNotFound, SaveFail)

---
+++ StagePainter/StagePainter.Core/Exceptions/AttributeNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagePainter.Core.Exceptions
+{
+    [Serializable]
+    public class AttributeNotFoundException : Exception
+    {
+        public AttributeNotFoundException() { }
+        public AttributeNotFoundException(string message) : base(message) { }
+        public AttributeNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected AttributeNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ StagePainter/StagePainter.Core/Exceptions/SaveFailException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagePainter.Core.Exceptions
+{
+    [Serializable]
+    public class SaveFailException : Exception
+    {
+        public SaveFailException() { }
+        public SaveFailException(string message) : base(message) { }
+        public SaveFailException(string message, Exception inner) : base(message, inner) { }
+        protected SaveFailException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
