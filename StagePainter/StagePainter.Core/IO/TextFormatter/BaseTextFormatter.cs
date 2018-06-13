fe78411b906e7e13e197f3dff5f66c87bdb8c463
Plurdis <uutak2000@naver.com>
2018-06-14 00:50:25 +0900

    Add TextFormatter (Base, XML)

---
+++ StagePainter/StagePainter.Core/IO/TextFormatter/BaseTextFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagePainter.Core.IO.TextFormatter
+{
+    public abstract class BaseTextFormatter
+    {
+        public BaseTextFormatter(object obj)
+        {
+            this.Object = obj;
+        }
+
+        public object Object { get; set; }
+
+        public abstract string TextFormat();
+    }
+}
---
+++ StagePainter/StagePainter.Core/IO/TextFormatter/XMLFormatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagePainter.Core.IO.TextFormatter
+{
+    public class XMLFormatter : BaseTextFormatter
+    {
+        public XMLFormatter(object obj) : base(obj)
+        {
+        }
+
+        public override string TextFormat()
+        {
+            return string.Empty;
+        }
+
+        // To-Do: Rebuild String Level Struct
+    }
+}
