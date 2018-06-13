7d82559536ae21baf4b9d99ef6beb265f56c55ec
Plurdis <uutak2000@naver.com>
2018-06-14 00:14:34 +0900

    Add 'ObjectEx' Class

---
+++ StagePainter/StagePainter.Core/Extension/ObjectEx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagePainter.Core.Extension
+{
+    public static class ObjectEx
+    {
+        public static T GetAttribute<T>(this object obj) where T : Attribute
+        {
+            Type type = obj.GetType();
+
+            return type.GetCustomAttributes(true)
+                       .Where(i => i is T)
+                       .FirstOrDefault() as T;
+        }
+
+        public static bool HasAttribute<T>(this object obj) where T : Attribute
+        {
+            return (obj.GetAttribute<T>() == null);
+        }
+    }
+}
