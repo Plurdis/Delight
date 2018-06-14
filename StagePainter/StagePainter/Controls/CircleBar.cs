82d1b6c4104049967417d73ea85b23c4fae545d1
Plurdis <uutak2000@naver.com>
2018-06-14 16:19:04 +0900

    Add 'CircleBar' Class Control

---
+++ StagePainter/StagePainter/Controls/CircleBar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StagePainter.Controls
+{
+    public class CircleBar : Control
+    {
+        private static DependencyProperty InnerValueProperty = InnerValuePropertyKey.DependencyProperty;
+        public static readonly DependencyPropertyKey InnerValuePropertyKey = DependencyProperty.RegisterReadOnly(nameof(InnerValue), typeof(double), typeof(CircleBar), new PropertyMetadata(default(double)));
+        public double InnerValue
+        {
+            get => (double)GetValue(InnerValueProperty);
+            private set => SetValue(InnerValuePropertyKey, value);
+        }
+    }
+}
