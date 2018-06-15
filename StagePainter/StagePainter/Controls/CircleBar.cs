b579be70a14636059adeae0a5bc8d26efc85d5f7
Plurdis <uutak2000@naver.com>
2018-06-15 13:40:47 +0900

    Modify 'CircleBar' Control

--- StagePainter/StagePainter/Controls/CircleBar.cs
+++ StagePainter/StagePainter/Controls/CircleBar.cs
@@ -5,17 +5,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace StagePainter.Controls
 {
+    [TemplatePart(Name ="piece", Type =typeof(PiePiece))]
     public class CircleBar : Control
     {
-        private static DependencyProperty InnerValueProperty = InnerValuePropertyKey.DependencyProperty;
-        public static readonly DependencyPropertyKey InnerValuePropertyKey = DependencyProperty.RegisterReadOnly(nameof(InnerValue), typeof(double), typeof(CircleBar), new PropertyMetadata(default(double)));
+        public CircleBar()
+        {
+            this.Style = FindResource("CircleBarStyle") as Style;
+        }
+
+        PiePiece piece;
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            piece = GetTemplateChild("piece") as PiePiece;
+        }
+
+        public static DependencyProperty InnerValueProperty = DependencyProperty.Register(nameof(InnerValue), 
+            typeof(double), 
+            typeof(CircleBar), 
+            new FrameworkPropertyMetadata(0.0, FrameworkPropertyMetadataOptions.AffectsArrange | FrameworkPropertyMetadataOptions.AffectsRender));
+
         public double InnerValue
         {
             get => (double)GetValue(InnerValueProperty);
-            private set => SetValue(InnerValuePropertyKey, value);
+            set => SetValue(InnerValueProperty, value);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            piece.WedgeAngle = InnerValue % 360;
         }
     }
 }
