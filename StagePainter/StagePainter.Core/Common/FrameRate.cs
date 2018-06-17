a8c7eba5af07758b4d16987fb822af8c430b71d6
Plurdis <uutak2000@naver.com>
2018-06-18 00:13:38 +0900

    Add 'FrameRate' Enum

---
+++ StagePainter/StagePainter.Core/Common/FrameRate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagePainter.Core.Common
+{
+    public enum FrameRate
+    {
+        [DefaultValue(24)]
+        _24FPS,
+        [DefaultValue(30)]
+        _30FPS,
+        [DefaultValue(60)]
+        _60FPS,
+    }
+}
