5e9d8e186ef187d7428213c39e29bbb0861f29e3
Plurdis <uutak2000@naver.com>
2018-06-19 00:40:47 +0900

    Add ScrollBar at TimeLine (Not Completed)

--- StagePainter/StagePainter.Debug/Controls/TimeLine.cs
+++ StagePainter/StagePainter.Debug/Controls/TimeLine.cs
@@ -11,40 +11,54 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
 namespace StagePainter.Debug.Controls
-{ 
+{
     [TemplatePart(Name = "trackSlider", Type = typeof(Rectangle))]
     [TemplatePart(Name = "trackPanel", Type = typeof(StackPanel))]
+    [TemplatePart(Name = "scrollBar", Type = typeof(ScrollBar))]
+    [TemplatePart(Name = "gridDrag", Type = typeof(Grid))]
     public class TimeLine : Control
     {
         public TimeLine()
         {
             this.MouseWheel += TimeLine_MouseWheel;
-            this.MouseDown += TimeLine_MouseDown;
             
             this.Style = FindResource("TimeLineStyle") as Style;
-
-            //int value = (int)FrameRate._24FPS.GetEnumAttribute<DefaultValueAttribute>().Value;
-            //MessageBox.Show(value.ToString());
         }
 
         public Rectangle trackSlider;
         public StackPanel trackPanel;
+        public ScrollBar scrollBar;
+        public Grid gridDrag;
 
         public List<Grid> tracks = new List<Grid>();
 
         public override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
+
             trackSlider = GetTemplateChild("trackSlider") as Rectangle;
             trackPanel = GetTemplateChild("trackPanel") as StackPanel;
+            scrollBar = GetTemplateChild("scrollBar") as ScrollBar;
+            gridDrag = GetTemplateChild("gridDrag") as Grid;
 
+            scrollBar.ViewportSize = 100;
+            scrollBar.Scroll += ScrollBar_Scroll;
+            gridDrag.MouseDown += GridDrag_MouseDown;
             AddTrack();
             AddTrack();
+        }
+
+        private void ScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            _offset = scrollBar.Value;
+            this.InvalidateVisual();
+            SetItemsValue();
         }
 
         public void AddTrack()
@@ -72,7 +86,6 @@ namespace StagePainter.Debug.Controls
         }
         
         Rectangle tempRect = null;
-
         int i = 0;
 
         private void Grid_DragLeave(object sender, DragEventArgs e)
@@ -103,8 +116,7 @@ namespace StagePainter.Debug.Controls
                 };
                 ((Grid)sender).Children.Add(tempRect);
             }
-                
-
+            
             Point relativePoint = this.TransformToAncestor((Visual)this.Parent)
                                       .Transform(new Point(0, 0));
 
@@ -115,8 +127,8 @@ namespace StagePainter.Debug.Controls
 
             double left = MouseManager.MousePosition.X - point.X;
 
-            int value = (int)(left / _realSize);
-            tempRect.Margin = new Thickness(value * _realSize, 0, 0, 0);
+            int value = (int)((left + _offset) / _realSize);
+            tempRect.Margin = new Thickness((int)(left / _realSize) * _realSize, 0, 0, 0);
 
             (tempRect.Tag as TestDataPack).Offset = value;
         }
@@ -145,7 +157,7 @@ namespace StagePainter.Debug.Controls
         public static DependencyProperty RatioProperty = DependencyProperty.Register(nameof(Ratio),
             typeof(double),
             typeof(TimeLine),
-            new FrameworkPropertyMetadata(12.8, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
+            new FrameworkPropertyMetadata(0.1, FrameworkPropertyMetadataOptions.AffectsMeasure | FrameworkPropertyMetadataOptions.AffectsRender));
 
         private const double MaxRatio = 12.8;
 
@@ -160,8 +172,10 @@ namespace StagePainter.Debug.Controls
                     value = MaxRatio;
 
                 SetValue(RatioProperty, value);
+
+                scrollBar.ViewportSize = value * 100;
+
                 SetItemsValue();
-                
             }
         }
 
@@ -181,23 +195,36 @@ namespace StagePainter.Debug.Controls
 
         private double _displaySize => _realSize * Weight;
 
+        private double _offset { get; set; }
+
         #endregion
 
         private void SetItemsValue()
         {
-            trackSlider.Margin = new Thickness((Value * _realSize) - 0.5, 0, 0, 0);
+            double max = 0;
+
+            trackSlider.Margin = new Thickness((Value * _realSize) - 0.5 - _offset, 0, 0, 0);
             foreach(Grid g in tracks)
             {
                 foreach(Rectangle rect in g.Children.Cast<UIElement>().Where(i => i.GetType() == typeof(Rectangle)))
                 {
                     TestDataPack pack = rect.Tag as TestDataPack;
-                    rect.Margin = new Thickness(pack.Offset * _realSize, 0, 0, 0);
-                    rect.Width = pack.Size * _realSize;
+
+                    double offset = pack.Offset * _realSize,
+                           width = pack.Size * _realSize;
+
+                    rect.Margin = new Thickness(offset - _offset, 0, 0, 0);
+                    rect.Width = width;
+                    max = Math.Max(max, offset + width - ActualWidth);
                 }
             }
+            if (max != 0)
+                scrollBar.Maximum = max;
+
+            scrollBar.Visibility = (max != 0) ? Visibility.Visible : Visibility.Hidden;
         }
 
-        private void TimeLine_MouseDown(object sender, MouseButtonEventArgs e)
+        private void GridDrag_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Thread thr = new Thread(() =>
             {
@@ -214,7 +241,7 @@ namespace StagePainter.Debug.Controls
 
                         var point = source.CompositionTarget.TransformFromDevice.Transform(locationFromScreen);
 
-                        double left = MouseManager.MousePosition.X - point.X;
+                        double left = MouseManager.MousePosition.X - point.X + _offset;
                         if (left < 0)
                             left = 0;
                         
@@ -283,6 +310,9 @@ namespace StagePainter.Debug.Controls
         
         public void DrawHelperLine(DrawingContext dc)
         {
+            double sizeOffset = _offset == 0 ? 0 : (_displaySize - (_offset % _displaySize)) - _displaySize;
+            int value = (int)(_offset / _displaySize);
+
             for (int i = 0; i <= ActualWidth / _displaySize; i++)
             {
                 var pen = new Pen(Brushes.Gray, 1);
@@ -291,32 +321,30 @@ namespace StagePainter.Debug.Controls
 
                 GuidelineSet guidelines = new GuidelineSet();
 
-                guidelines.GuidelinesX.Add((i * _displaySize) + halfPenWidth);
-                guidelines.GuidelinesX.Add((i * _displaySize) + _displaySize + halfPenWidth);
-                //guidelines.GuidelinesY.Add(65.5);
-
+                guidelines.GuidelinesX.Add((i * _displaySize) + sizeOffset + halfPenWidth);
+                guidelines.GuidelinesX.Add((i * _displaySize) + sizeOffset + _displaySize + halfPenWidth);
+                
                 dc.PushGuidelineSet(guidelines);
                 int height = 10;
-                if (i % 12 == 0)
+                if ((i + value) % 12 == 0)
                 {
                     height = 28;
                     pen.Brush = Brushes.Black;
-                    dc.DrawText(new FormattedText(GetTimeText(i * Weight), CultureInfo.GetCultureInfo("en-us"),
+                    dc.DrawText(new FormattedText(GetTimeText((i + value) * Weight), CultureInfo.GetCultureInfo("en-us"),
                         FlowDirection.LeftToRight,
                         new Typeface("Arial"),
                         10, Brushes.Black),
-                        new Point(i * _displaySize + 4, 2));
+                        new Point(i * _displaySize + sizeOffset + 4, 2));
 
                     guidelines.GuidelinesY.Add(35.5 - height);
-                    dc.DrawLine(pen, new Point(i * _displaySize, 35 - height), new Point(i * _displaySize + 3, 35 - height));
+                    dc.DrawLine(pen, new Point(i * _displaySize + sizeOffset, 35 - height), new Point(i * _displaySize + sizeOffset + 3, 35 - height));
                 }
-                else if (i % 4 == 0)
+                else if ((i + value) % 4 == 0)
                 {
                     height = 20;
                     pen.Brush = Brushes.Black;
                 }
-                dc.DrawLine(pen, new Point(i * _displaySize, 35 - height), new Point(i * _displaySize, 35));
-
+                dc.DrawLine(pen, new Point(i * _displaySize + sizeOffset, 35 - height), new Point(i * _displaySize + sizeOffset, 35));
                 dc.Pop();
             }
         }
