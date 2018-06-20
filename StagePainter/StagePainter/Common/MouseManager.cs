9ddfaa5ee82c56b644ec048bfbd3a5bc6f6e9e38
Plurdis <uutak2000@naver.com>
2018-06-20 14:51:29 +0900

    Move 'TimeLine' Control to 'StagePainter' (from StagePainter.Debug)

---
+++ StagePainter/StagePainter/Common/MouseManager.cs
@@ -0,0 +1,46 @@
+﻿using Gma.System.MouseKeyHook;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StagePainter.Common
+{
+    public static class MouseManager
+    {
+        static MouseManager()
+        {
+            Event = Hook.GlobalEvents();
+
+            Event.MouseDown += Event_MouseDown;
+            Event.MouseUp += Event_MouseUp;
+        }
+
+        static IMouseEvents Event;
+
+        public static void Init()
+        {
+        }
+
+        public static bool IsMouseDown { get; private set; }
+
+        public static Point MousePosition => Control.MousePosition;
+
+        #region [  Added Event  ]
+
+        private static void Event_MouseUp(object sender, MouseEventArgs e)
+        {
+            IsMouseDown = false;
+        }
+
+        private static void Event_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsMouseDown = true;
+        }
+
+        #endregion
+    }
+}
---
+++ StagePainter/StagePainter/Tracks/TrackItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+using StagePainter.Controls;
+
+namespace StagePainter.Tracks
+{
+    /// <summary>
+    /// Represents a control that move in <see cref="TimeLine"/> Tracks.
+    /// </summary>
+    public class TrackItem : Control
+    {
+        
+    }
+}
