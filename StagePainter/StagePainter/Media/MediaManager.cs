db0c17b40527bbbd535a6d3c2ae726b7b5279e9c
Plurdis <uutak2000@naver.com>
2018-07-01 11:59:40 +0900

    Add ProjectInfo Class

--- StagePainter/StagePainter/Media/MediaManager.cs
+++ StagePainter/StagePainter/Media/MediaManager.cs
@@ -11,15 +11,45 @@ namespace StagePainter.Media
     /// </summary>
     public class MediaManager
     {
+        #region [  Static Members  ]
+
+        public Image GetImageFromPath(string filePath)
+        {
+            return null;
+        }
+
+        public Sound GetSoundFromPath(string filePath)
+        {
+            return null;
+        }
+
+        public Video GetVideoFromPath(string filePath)
+        {
+            return null;
+        }
+
+        #endregion
+
+        private Dictionary<string, IMedia> _medias;
+
         public void AddImage(Image image)
         {
-
+            AddMedia(image);
         }
 
         public void AddSound(Sound sound)
         {
-
+            AddMedia(sound);
         }
 
+        public void AddVideo(Video video)
+        {
+            AddMedia(video);
+        }
+
+        private void AddMedia(IMedia media)
+        {
+            _medias[media.Identity] = media;
+        }
     }
 }
---
+++ StagePainter/StagePainter/Projects/ProjectInfo.cs
@@ -0,0 +1,23 @@
+﻿using StagePainter.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StagePainter.Projects
+{
+    /// <summary>
+    /// Represents Project Resources and about project datas.
+    /// </summary>
+    public class ProjectInfo
+    {
+        public ProjectInfo()
+        {
+            MediaManager = new MediaManager();
+        }
+
+        public MediaManager MediaManager { get; }
+
+    }
+}
