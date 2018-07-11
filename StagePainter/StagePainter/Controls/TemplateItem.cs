cf1815fd935044be85e33b98e49be36541e34f65
Plurdis <uutak2000@naver.com>
2018-07-11 23:32:57 +0900

    TemplateItem 컨트롤 추가 및 MainWindow에 요소 추가

---
+++ StagePainter/StagePainter/Controls/TemplateItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace StagePainter.Controls
+{
+    public class TemplateItem : ListBoxItem
+    {
+        public TemplateItem()
+        {
+            this.Style = FindResource("TemplateItemStyle") as Style;
+        }
+
+        public static DependencyProperty DescriptionProperty = DependencyProperty.Register(nameof(Description), typeof(string), typeof(TemplateItem));
+        public string Description
+        {
+            get => GetValue(DescriptionProperty) as string;
+            set => SetValue(DescriptionProperty, value);
+        }
+    }
+}
