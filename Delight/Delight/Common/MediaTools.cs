9a86e306511067ab436c54a2e849a13f5afe307e
Plurdis <uutak2000@naver.com>
2018-07-15 22:30:03 +0900

    MediaTools에서 확장자에 따른 MediaTypes를 가져오는 GetMediaTypeFromFile 함수 추가

--- Delight/Delight/Common/MediaTools.cs
+++ Delight/Delight/Common/MediaTools.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.WindowsAPICodePack.Shell;
-using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Delight.Media;
+using Microsoft.WindowsAPICodePack.Shell;
+using Microsoft.WindowsAPICodePack.Shell.PropertySystem;
 
 namespace Delight.Common
 {
@@ -18,6 +20,35 @@ namespace Delight.Common
                 var t = (ulong)prop.ValueAsObject;
                 return TimeSpan.FromTicks((long)t);
             }
+        }
+
+        
+        public static MediaTypes GetMediaTypeFromFile(string fileName)
+        {
+            string extension = new FileInfo(fileName).Extension;
+            switch (extension)
+            {
+                case ".jpg":
+                case ".bmp":
+                case ".png":
+                case "jpeg":
+                case "gif":
+                    return MediaTypes.Image;
+                case ".wav":
+                case ".mp3":
+                case ".flac":
+                case ".m4a":
+                    return MediaTypes.Sound;
+                case ".avi":
+                case "mpeg":
+                case "mp4":
+                case ".mov":
+                case ".qt":
+                    return MediaTypes.Video;
+                default:
+                    break;
+            }
+            return MediaTypes.Unknown;
         }
     }
 }
