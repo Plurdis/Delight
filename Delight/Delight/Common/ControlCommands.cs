e72f1b0f9944b868fd057f2781336e0384604103
Plurdis <uutak2000@naver.com>
2018-07-24 02:14:57 +0900

    여러가지 신호들을 컨트롤할 ControlCommand 추가

---
+++ Delight/Delight/Common/ControlCommands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Delight.Common
+{
+    public static class ControlCommands
+    {
+        public static RoutedCommand PlayCommand { get; }
+
+        static ControlCommands()
+        {
+            PlayCommand = new RoutedCommand("PlayCommand", typeof(ControlCommands));
+        }
+    }
+}
