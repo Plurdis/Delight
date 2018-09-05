e932c28b9cb9cf389dcf573e1108a5447bd390e2
Plurdis <uutak2000@naver.com>
2018-09-05 20:42:06 +0900

    'DelightTemplate'파일과 'ZipManager'파일 추가

---
+++ Delight/Delight.Core/Template/Common/DelightTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delight.Core.Template.Common
+{
+    public class DelightTemplate
+    {
+        public DelightTemplate()
+        {
+
+        }
+    }
+}
---
+++ Delight/Delight.Core/Template/Common/ZipManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace Delight.Core.Template.Common
+{
+    public static class ZipManager
+    {
+        // TODO: 함수 완성
+        public static DelightTemplate GetDelightTemplate(string fileName)
+        {
+            ZipArchive archive = ZipFile.OpenRead(fileName);
+            var entries = archive.GetEntry("template.list");
+
+            Stream stream = entries.Open();
+            StreamReader stReader = new StreamReader(stream);
+
+            Console.WriteLine(stReader.ReadToEnd());
+
+            return null;
+        }
+    }
+
+}
